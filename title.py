46111b48b5e33063a2210e0e320ca51149ea67b8
dmitrii12335 <145354732+dmitrii12335@users.noreply.github.com>
2024-03-03 18:19:33 +0000

    hello

--- title.py
+++ title.py
@@ -1,13 +1,14 @@
 
 class LQR:
-    def __init__(self, H, M, x, sigma, C, D, T):
+    def __init__(self, H, M, x, sigma, C, D, T, R):
         self.H = H
         self.M = M
         self.x = x
-        self.sigma = sigma
+        self.s = sigma
         self.C = C
         self.D = D
         self.T = T
+        self.R = R
 
     def riccati_equation(self, time, value_T):
         
