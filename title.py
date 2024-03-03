0a79e8c6f103a0f0fd792f452e655c6c1ad598be
axelk99 <145706322+axelk99@users.noreply.github.com>
2024-03-03 18:16:16 +0000

    initialise class

--- title.py
+++ title.py
@@ -1,1 +1,13 @@
-hello
+
+class LQR:
+    def __init__(self, H, M, x, sigma, C, D, T):
+        self.H = H
+        self.M = M
+        self.x = x
+        self.sigma = sigma
+        self.C = C
+        self.D = D
+        self.T = T
+
+    def riccati_equation(self, time, value_T):
+        
