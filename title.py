0d43d58fa25248022e3352cd33e83a28db7038c4
dmitrii12335 <145354732+dmitrii12335@users.noreply.github.com>
2024-03-03 19:42:21 +0000

    hello

--- title.py
+++ title.py
@@ -10,5 +10,19 @@ class LQR:
         self.T = T
         self.R = R
 
-    def riccati_equation(self, time, value_T):
+    def riccati_equation(self, time):
+        time_rev = time[::-1]
+        L = len(time)
+        res = [self.R]
+        D_inv = np.linalg.inv(self.D)
+
+        for i in range(L-1):
+            S = res[-1]
+            delta = time_rev[i] - time_rev[i+1]
+            if i == 0:
+                print(delta)
+            S_new = ( (self.H.T) @ S - S @ self.M @ D_inv @ self.M @ S + self.C + S ) * delta + S
+
+            res.append(S_new)
+        return res
         
